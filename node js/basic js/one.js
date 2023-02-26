6b3255f43f26ce351ca6ed04ccb2c3a99936c4b0
roopahn184 <rooparoopahn184@gmail.com>
2023-02-26 17:27:46 +0530

    ok

---
+++ node js/basic js/one.js
@@ -0,0 +1,4 @@
+console.log("req 1");
+console.log("req 2");
+console.log("req 3");
+console.log("req 4");
---
+++ node js/os/one.js
@@ -0,0 +1,7 @@
+const os = require('os')
+//import os from 'os'
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.hostname());

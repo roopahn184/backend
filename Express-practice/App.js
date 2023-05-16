b373171b7fc87d2a611d9d0b53601fe2ee8a6ab7
roopahn184 <rooparoopahn184@gmail.com>
2023-05-16 16:28:57 +0530

    ok

--- Express-practice/App.js
+++ Express-practice/App.js
@@ -3,7 +3,13 @@ var app =express()
 
 app.get("/",(req, resp)=>{
   resp.send("Root request")
-})
+});
+app.get("/user", (req, resp)=>{
+  resp.send("User Request")
+});
+app.get("/employee",(req, resp)=>{
+  resp.send(`<h1>Employee Data </h1>`)
+});
 
 app.listen(5000,(err)=>{
     if (err) throw err
